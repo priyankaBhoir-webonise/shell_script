22645e15fbb7612ec4aca679305346dc834a989a
webonise <webonise@webonise-desktop.(none)>
2013-06-27 18:51:42 +0530

    after second question_changes

--- assign6_2.sh
+++ assign6_2.sh
@@ -2,7 +2,7 @@ echo " rename all text files :`rename 's/^/abc_/'  *.txt && ls`  updated on `dat
 echo " rename all  files starting with x `rename 's/^x/abc_/'  *.* && ls`   updated on `date` " |tee -a assignment.log
 mkdir public_html 
 cd public_html
-touch reame.txt
+touch readme.txt
 chmod 777 readme.txt
 cd ..
-ls -s| head -4 | xargs -I{} mv {} ./public_html
+ls -s| head -4  ## get 4 file after sorting
